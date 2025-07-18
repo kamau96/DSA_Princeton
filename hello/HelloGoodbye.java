eb108629ef107952b6a192703fab643899fadb18
Kamau Wainaina <benson.kamau1738@gmail.com>
2025-07-18 15:11:36 +0300

    This is the first commit. I'll learn algorithms and data structures to help me write efficient programs.

---
+++ hello/HelloGoodbye.java
@@ -0,0 +1,9 @@
+public class HelloGoodbye {
+    public static void main(String[] args) {
+        System.out.println("Hello "+args[0]+ " and "+ args[1] + ".");
+        System.out.println("Goodbye "+args[1]+ " and "+ args[0] + ".");
+    }
+    
+}
+// This Java program takes two command-line arguments and prints a greeting and farewell message.
+// It demonstrates how to handle command-line arguments in a Java application.
---
+++ hello/RandomWord.java
@@ -0,0 +1,23 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+    public static void main(String[] args) {
+        String word = null;
+        int count = 0;
+
+        while (!StdIn.isEmpty()) {
+            String current = StdIn.readString(); 
+            count++;
+            if (StdRandom.bernoulli(1.0 / count)) {
+                word = current;  
+            }
+        }
+
+        StdOut.println(word);
+
+    }
+}
+// This program reads words from standard input and randomly selects one word
+// with a uniform probability. The more words read, the lower the chance of 
